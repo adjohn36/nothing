f7565e1ee221b565cc256f126078205216061ff6
adjohn36 <132848782+adjohn36@users.noreply.github.com>
2024-06-10 11:37:44 -0400

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,30 @@
+export const scaleFactor = 3;
+
+export const dialogueData = {
+    pc: `This is where I get to work!  I work in Java, SQL, HTML, CSS, and JavaScript.  
+    I'm working towards learning Python in the near future.
+    Here is the link to my <a href="https://github.com/adjohn36" target="_blank">GitHub Profile</a>.  
+    `,
+    tv: `I don't watch alot of TV, but when I do it's usually Philly sports 
+    (Eagles, Phillies, Sixers), or shows that my daughter enjoys 
+    (Doc McStuffins, Bluey, Mickey Mouse Clubhouse, etc.).`,
+    bed: `Sleep? Do Developers get that?`,
+    resume: `Here is a link to my <a href="https://www.linkedin.com/in/adamjohnson00001/details/featured/1717974073466/single-media-viewer/?type=DOCUMENT&profileId=ACoAAEM806YBpAyz9fJECnJaMu45o0SFqICF_as"
+     target="_blank">Resume</a>, I hope you like what you see!`,
+    "sofa-table": `My couch.  This is where I get to relax.  I recently made a retro gaming system with a 
+    <a href="https://www.raspberrypi.com/" target="_blank">Raspberry Pi</a> that I play here when I get some free time.`,
+    projects: `Here is a link to my personal page with some of my favorite 
+    <a href="https://adjohn36.github.io/" target=_blank">projects</a>.`,
+    library: `There's too many books to name on my shelves, but some classics you might know of are
+    Rich Dad, Poor Dad, 7 Habits of Highly Effective People, and the Game of Thrones series.`,
+    exit: "Don't leave yet!  You didn't get the tour!",
+    certifications: `I graduated from Tech Elevator, a full stack web development bootcamp, in August 2023.  
+    My coding languages of focus were Java, SQL, HTML, CSS, and JavaScript, while also utilizing many different programs in order to write, test, and debug code.
+    It was a 14 week bootcamp where we learned how to create a dynamic web-based software system.  The course provided 800+ hours of development education and application.
+    Here is my <a href="https://media.licdn.com/dms/image/D4E2DAQHWVBAQfOvdfg/profile-treasury-image-shrink_160_160/0/1717971628743?e=1718578800&v=beta&t=um6NIZ4467LLTTIgzJ_p8sU_-QtRvzLwjA8tuYbkTh8" target="_blank">certificate</a>.
+    <br></br>I've also been learning through <a href="https://www.freecodecamp.org/" target="_blank">freeCodeCamp</a> 
+    and have earned certifications in <a href="https://www.freecodecamp.org/certification/adjohn36/responsive-web-design" target="-blank">Responsive Web Design</a> 
+    and <a href="https://www.freecodecamp.org/certification/adjohn36/javascript-algorithms-and-data-structures-v8" target="_blank">JavaScript Algorithms and Data Structures</a>.  
+    More to come soon!`
+    
+};
